5ee51a09ef8cc0db1693ddc62e57bfe3c7724053
Caitlin Greer <clento1121@gmail.com>
2021-03-12 10:55:13 -0500

    created store for data

---
+++ src/store.js
@@ -0,0 +1,9 @@
+const bookmarks = [{
+    title: "Google",
+    url: "www.google.com",
+    description: "The answer to every question...",
+    rating: 4,
+    id: 1
+}]
+
+module.exports = bookmarks
