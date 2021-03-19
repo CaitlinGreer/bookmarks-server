c08e109a3db6e5f73052878039a12a52e1af9143
Caitlin Greer <clento1121@gmail.com>
2021-03-19 09:45:54 -0400

    script to seed database

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,7 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+    ('Google', 'www.google.com', 'Where the internet provides all the answers', 5),
+    ('Facebook', 'www.facebook.com', 'Where your parents share dumb things and family fights over politics', 2),
+    ('GitHub', 'www.github.com', 'A place to store all your code', 4),
+    ('Instagram', 'www.instagram.com', 'Overly edited food/fashion/sunset photos', 2),
+    ('Thinkful', 'www.thinkful.com', 4);
