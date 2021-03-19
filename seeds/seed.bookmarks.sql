fea5434be75d126a04ed7cb43b1272f1723d3abc
Caitlin Greer <clento1121@gmail.com>
2021-03-19 09:48:59 -0400

    created data to seed table

--- seeds/seed.bookmarks.sql
+++ seeds/seed.bookmarks.sql
@@ -4,4 +4,4 @@ VALUES
     ('Facebook', 'www.facebook.com', 'Where your parents share dumb things and family fights over politics', 2),
     ('GitHub', 'www.github.com', 'A place to store all your code', 4),
     ('Instagram', 'www.instagram.com', 'Overly edited food/fashion/sunset photos', 2),
-    ('Thinkful', 'www.thinkful.com', 4);
+    ('Thinkful', 'www.thinkful.com', 'Learn to code', 4);
