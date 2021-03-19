8dc5434eac012ec0b79b8d1b828354f62f8bd3cf
Caitlin Greer <clento1121@gmail.com>
2021-03-19 13:49:44 -0400

    wrote tests for when there are no bookmarks

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -21,21 +21,52 @@ describe.only('Bookmarks Endpoints', function () {
 
     afterEach('cleanup', () => db('bookmarks').truncate())
 
-    context('Given there are bookmarks in the database', () => {
-        const testBookmarks = makeBookmarksArray()
-
-        beforeEach('insert bookmarks', () => {
-            return db
-            .into('bookmarks')
-            .insert(testBookmarks)
-        })
-
-        it('GET /bookmarks responds with 200 and all of the bookmarks', () => {
-            return supertest(app)
+    describe(`GET /bookmarks`, () => {
+        context ('Given no bookmarks', () => {
+            it(`responds with 200 and an empty list`, () => {
+                return supertest(app)
                 .get('/bookmarks')
                 .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
-                .expect(200, testBookmarks)
+                .expect(200, [])
+            })
         })
+        context('Given there are bookmarks in the database', () => {
+            const testBookmarks = makeBookmarksArray()
+
+            beforeEach('insert bookmarks', () => {
+                return db
+                .into('bookmarks')
+                .insert(testBookmarks)
+            })
+
+            it('GET /bookmarks responds with 200 and all of the bookmarks', () => {
+                return supertest(app)
+                    .get('/bookmarks')
+                    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                    .expect(200, testBookmarks)
+            })
+        })
+    })
+
+    describe(`GET /bookmarks/:id`, () => {
+        context(`Given no bookmarks`,  () => {
+            it(`responds with 404`, () => {
+                const bookmarkId = 12345
+                return supertest(app)
+                    .get(`/bookmarks/${bookmarkId}`)
+                    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                    .expect(404, { error: { message: `Bookmark doesn't exist` }})
+            })
+        })
+
+        context('Given there are bookmarks in the database', () => {
+            const testBookmarks = makeBookmarksArray()
+
+            beforeEach('insert bookmarks', () => {
+                return db
+                    .into('bookmarks')
+                    .insert(testBookmarks)
+            })
         it ('GET /bookmarks/:id responds with 200 and the specified bookmark', () => {
             const bookmarkId = 2
             const expectedBookmark = testBookmarks[bookmarkId - 1]
@@ -43,6 +74,7 @@ describe.only('Bookmarks Endpoints', function () {
                 .get(`/bookmarks/${bookmarkId}`)
                 .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
                 .expect(200, expectedBookmark)
+            })
         })
     })
 })
