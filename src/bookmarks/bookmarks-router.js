a81d9d77df466ed070a75792a806d4e0ce7cbcbd
Caitlin Greer <clento1121@gmail.com>
2021-03-24 11:30:30 -0400

    required path adjustled location to fix possible //

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const xss = require('xss')
 const { v4: uuid } = require('uuid')
@@ -54,48 +55,71 @@ bookmarksRouter // /bookmarks route
           logger.info(`Bookmark with id ${bookmark.id} created`)
           res
             .status(201)
-            .location(`/bookmarks/${bookmark.id}`)
+            .location(path.posix.join(req.originalUrl, `/${bookmark.id}`))
             .json(serializeBookmarks(bookmark))
         })
         .catch(next)
     })
  
-    bookmarksRouter
-        .route('/:id')
-        .all((req, res, next) => {
-            BookmarksService.getById(
-                req.app.get('db'),   
-                req.params.id
-            )
-            .then(bookmark => {
-                if(!bookmark) {
-                    return res.status(404).json({
-                        error: { message: `Bookmark doesn't exist` }
-                    })
-                }
-                res.bookmark = bookmark
-                next()
+bookmarksRouter
+  .route('/:id')
+    .all((req, res, next) => {
+        BookmarksService.getById(
+            req.app.get('db'),   
+            req.params.id
+    )
+    .then(bookmark => {
+        if(!bookmark) {
+            return res.status(404).json({
+              error: { message: `Bookmark doesn't exist` }
             })
-            .catch(next)
-            
+        }
+            res.bookmark = bookmark
+              next()
         })
+        .catch(next)
+    })
 
-        .get((req, res, next) => {
-            res.json(serializeBookmarks(res.bookmark))
+    .get((req, res, next) => {
+        res.json(serializeBookmarks(res.bookmark))
+    })
+
+    .delete((req, res, next) => {
+        const { id } = req.params
+
+    BookmarksService.deleteBookmark(
+        req.app.get('db'),
+            id
+        )
+        .then(() => {
+            res.status(204).end()
         })
+        .catch(next)
+    })
+    
+    .patch(bodyParser, (req, res, next) => {
+        const { title, url, description, rating } = req.body
+        const bookmarkToUpdate = { title, url, description, rating }
 
-        .delete((req, res, next) => {
-            const { id } = req.params
-
-            BookmarksService.deleteBookmark(
-                req.app.get('db'),
-                id
-            )
-            .then(() => {
-                res.status(204).end()
-            })
-            .catch(next)
-        })
+        const numberOfValues = Object.values(bookmarkToUpdate).filter(Boolean).length
+        if (numberOfValues === 0) {
+          return res.status(400).json({
+            error: {
+              message: `Request body must contain either 'title', 'url', 'description' or 'rating'`
+            }
+          })
+        }
+        
+        BookmarksService.updateBookmark(
+          req.app.get('db'),
+          req.params.id,
+          bookmarkToUpdate
+        )
+          .then(numRowsAffected => {
+            res.status(204).end()
+          })
+          .catch(next)
+    })
         
 
-    module.exports = bookmarksRouter
+module.exports = bookmarksRouter
